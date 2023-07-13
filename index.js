35a099fe6d0556b2a39868bf25de4da78e8e6ad0
eduardodea85 <eduardodea@outlook.com>
2023-07-13 00:31:19 -0300

    Criando primeira api

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const server = express();
+
+//localhost:3000/curso
+server.get('/curso', (req, res) => {
+    //return res.send('Hello Word')
+    return res.json({curso:'Node JS'})
+})
+
+server.listen(3000);
