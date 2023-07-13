50b2885b2a18aa511a6899031d312179572f1029
eduardodea85 <eduardodea@outlook.com>
2023-07-13 00:43:08 -0300

    Entendendo query, routes, params

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 
 const server = express();
 
-//localhost:3000/curso
-server.get('/curso', (req, res) => {
+// Query Params = ?nome=NodeJS
+// Route Params = /curso/2
+// Request Body = { nome: 'NodeJS', tipo: 'Backend' }
+
+//localhost:3000/curso/2
+server.get('/curso/:id', (req, res) => {
+    //const nome = req.query.nome;
+    const id = req.params.id;
+
+
     //return res.send('Hello Word')
-    return res.json({curso:'Node JS'})
+    //return res.json({curso: `Aprendendo ${nome}`})
+    return res.json({ curso: `Curso ${id}`})
 })
 
 server.listen(3000);
