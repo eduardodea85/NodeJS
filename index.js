f6a838888e7e884188f231c3405b8208a10f9095
eduardodea85 <eduardodea@outlook.com>
2023-07-16 22:54:18 -0300

    Usando insomnia

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ const server = express();
 // Route Params = /curso/2
 // Request Body = { nome: 'NodeJS', tipo: 'Backend' }
 
+const cursos = ['Node JS', 'JavaScript', 'React Native'];
+
 //localhost:3000/curso/2
-server.get('/curso/:id', (req, res) => {
+//server.get('/curso/:id', (req, res) => {
+    server.get('/curso/:index', (req, res) => {
     //const nome = req.query.nome;
-    const id = req.params.id;
+    //const id = req.params.id;
+    const {index} = req.params;
 
 
     //return res.send('Hello Word')
     //return res.json({curso: `Aprendendo ${nome}`})
-    return res.json({ curso: `Curso ${id}`})
+    //return res.json({ curso: `Curso ${id}`})
+    return res.json(cursos[index]);
 })
 
 server.listen(3000);
