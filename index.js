d7a89acffc29e7ef84845f94556d43df746bbf0f
eduardodea85 <eduardodea@outlook.com>
2023-07-16 23:35:01 -0300

    Praticando conhecimento - CRUD

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());//quando enviamos um json na aplicação (POST), precisamos falar para o express que estamos enviando uma estrutura JSON.
+
 // Query Params = ?nome=NodeJS
 // Route Params = /curso/2
 // Request Body = { nome: 'NodeJS', tipo: 'Backend' }
 
+//CRUD> Create, Read, Update, Delete
+
 const cursos = ['Node JS', 'JavaScript', 'React Native'];
+
+server.get('/cursos', (req, res) => {
+    return res.json(cursos);
+});
 
 //localhost:3000/curso/2
 //server.get('/curso/:id', (req, res) => {
-    server.get('/curso/:index', (req, res) => {
+    server.get('/cursos/:index', (req, res) => {
     //const nome = req.query.nome;
     //const id = req.params.id;
     const {index} = req.params;
@@ -20,6 +28,28 @@ const cursos = ['Node JS', 'JavaScript', 'React Native'];
     //return res.json({curso: `Aprendendo ${nome}`})
     //return res.json({ curso: `Curso ${id}`})
     return res.json(cursos[index]);
-})
+});
+
+//Criando um novo curso
+server.post('/cursos', (req, res) => {
+    const {name} = req.body;
+    cursos.push(name);
+    return res.json(cursos);
+});
+
+//Atualizando um curso
+server.put('/cursos/:index', (req, res) => {
+    const {index} = req.params;
+    const {name} = req.body;
+    cursos[index] = name;
+    return res.json(cursos);
+});
+
+//Excluindo um curso
+server.delete('/cursos/:index', (req, res) => {
+    const {index} = req.params;
+    cursos.splice(index, 1);
+    return res.json({message: "Curso deletado com sucesso!"});
+});
 
 server.listen(3000);
